c3455d550c681688d1350d9585375915e702735a
Richard <richard.rabeau@gmail.com>
2015-10-30 20:14:30 +0000

    price added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,10 +69,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
+      params.require(:product).permit(:name, :description, :image_url, :color, :price)
     end
-    
-    def product_params
-     params.require(:product).permit(:name, :description, :image_url, :color)
-    end
+  
 end
