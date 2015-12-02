879cb047254c890bb2a9063f562912068abc5df7
Richard <richard.rabeau@gmail.com>
2015-12-02 17:30:43 +0000

    email respince added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,17 @@
 class StaticPagesController < ApplicationController
   
-    def index
-    end
-   
-    def landing_page
-      @products = Product.limit(4)
-    end
-    
-    def thank_you
-      @name = params[:name]
-      @email = params[:email]
-      @message = params[:message]
-      ActionMailer::Base.mail(:from => @email,
-          :to => 'richard.rabeau@gmail.com',
-          :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-    end
+  def index
+  end
+ 
+  def landing_page
+    @products = Product.limit(4)
+  end
+  
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
   
 end
