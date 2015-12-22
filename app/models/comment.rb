6ff1f7cfa06529f36fc9637e3e1414a6844770eb
Richard <richard.rabeau@gmail.com>
2015-12-22 20:59:12 +0000

    comment requirements added/pagenation installed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,24 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
+  
+  def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@
 class Product < ActiveRecord::Base
   has_many :orders
   has_many :comments
+  validates :name, presence: true
 end
